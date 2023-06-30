bf1c2ba1f8e7119a895ba70918c591360d0ada69
EISAGHANI <eisabacha.com>
2023-06-30 16:50:48 +0200

    commit

---
+++ src/pages/VideoComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import videoFile from './assets/1_entwurf.mp4';
+import './VideoComponent.css'; // Import the CSS file for styling
+
+const VideoComponent = () => {
+  return (
+    <div className="video-container">
+      <video className="video-element" loop autoPlay>
+        <source src={videoFile} type="video/mp4" />
+          Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default VideoComponent;
