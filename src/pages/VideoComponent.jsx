f78382b75ae4e8f3370b8066939f9fdded3b0392
EISAGHANI <eisabacha.com>
2023-07-03 12:31:47 +0200

    changes

--- src/pages/VideoComponent.jsx
+++ src/pages/VideoComponent.jsx
@@ -5,7 +5,7 @@ import './VideoComponent.css'; // Import the CSS file for styling
 const VideoComponent = () => {
   return (
     <div className="video-container">
-      <video className="video-element" loop autoPlay>
+      <video className="video-element" loop autoPlay muted >
         <source src={videoFile} type="video/mp4" />
           Your browser does not support the video tag.
       </video>
